80158afa462a1c175fdb2fbfbf68fbd417069355
stratope <casa.amaru2024@gmail.com>
2026-04-14 18:33:02 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
-    const URL_PROPIEDADES = "https://docs.google.com/spreadsheets/d/1VctscCRyoQ-sdWa1vlGG0xsjjGY5Jznw6LaK20syz3g/export?format=csv&gid=0";
-    const URL_CONFIG = "https://docs.google.com/spreadsheets/d/1VctscCRyoQ-sdWa1vlGG0xsjjGY5Jznw6LaK20syz3g/export?format=csv&gid=563916861";
+    const URL_PROPIEDADES = "https://docs.google.com/spreadsheets/d/1zzr_ZsJ4gJedO9qin0vSjwJNGEMZvPg7uzymuYxGTf0/export?format=csv";
+    const URL_CONFIG = "https://docs.google.com/spreadsheets/d/1zzr_ZsJ4gJedO9qin0vSjwJNGEMZvPg7uzymuYxGTf0/export?format=csv&gid=978902660";
 
     try {
         const [resProp, resConfig] = await Promise.all([
